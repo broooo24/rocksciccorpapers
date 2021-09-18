1fa024c553debc43654147a8a7797b880588b441
broooo24 <84236471+broooo24@users.noreply.github.com>
2021-09-18 21:08:56 +0200

    Add files via upload

---
+++ rockpapersciccors/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("First player: ");
+        String str1 = scanner.nextLine();
+        System.out.print("Second player: ");
+        String str2 = scanner.nextLine();
+
+        //write in lowercase
+        String user1 = str1.toLowerCase();
+        String user2 = str2.toLowerCase();
+
+        if(user1.equals("paper")&&user2.equals("rock")||user1.equals("rock")&&user2.equals("sciccors")||user1.equals("sciccors")&&user2.equals("paper"))
+        System.out.println("1st win");
+
+        if(user2.equals("paper")&&user1.equals("rock")||user2.equals("rock")&&user1.equals("sciccors")|| user2.equals("sciccors")&&user1.equals("paper"))
+        System.out.println("2nd win");
+
+        if (user1.equals(user2))
+            System.out.println("Draw");
+
+        //String s1 = "a" String s2= "a"
+    }
+}
